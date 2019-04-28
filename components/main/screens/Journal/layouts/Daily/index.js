cc5dfe8c66adf0ee3a4b2c1715b1c9a98ecdc4bd
minhchinh97h <minhchinhduong97@gmail.com>
2019-04-28 23:21:10 +0300

    Update index.js

--- components/main/screens/Journal/layouts/Daily/index.js
+++ components/main/screens/Journal/layouts/Daily/index.js
@@ -6,10 +6,13 @@ import {
     StatusBar,
     SafeAreaView,
     Button,
-    ScrollView
+    ScrollView,
+    TouchableHighlight
 } from 'react-native';
 
-let scrollViewRef
+let scrollViewRef,
+    dayHolderWidth = 60
+    
 
 export default class Daily extends React.Component{
     static navigationOptions = {
@@ -69,34 +72,81 @@ export default class Daily extends React.Component{
         }
 
         this.setState({
-            dailyTimeView: days_arr.map((obj, index) => (
-                <View style={styles.dayHolder} key={obj + " " + index}>
-                    <Text>{obj.dayWord}</Text>
-                    <Text>{obj.dayNumb}</Text>
-                </View>
-            ))
+            dailyTimeView: days_arr.map((obj, index) => {
+                if(obj.dayNumb === today){
+                    return (
+                        <TouchableHighlight onPress={() => console.log("true")} style={styles.dayHolder} key={obj + " " + index}>
+                        <>
+                            <View>
+                                <Text>{obj.dayWord}</Text>
+                            </View>
+
+                            <View style={styles.currentDayHolder}>
+                                <Text 
+                                    style={{
+                                        color: 'white'
+                                    }}
+                                >
+                                    {obj.dayNumb}
+                                </Text>
+                            </View>
+                        </>
+                        </TouchableHighlight>
+                    )
+                }
+                
+                else{
+                    return (
+                        <TouchableHighlight onPress={() => console.log("true")} style={styles.dayHolder} key={obj + " " + index}>
+                        <>
+                            <View>
+                                <Text>{obj.dayWord}</Text>
+                            </View>
+
+                            <View 
+                                style={{
+                                    height: 35,
+                                    width: dayHolderWidth,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <Text>{obj.dayNumb}</Text>
+                            </View>
+                        </>
+                        </TouchableHighlight>
+                    )
+                }
+            })
         })
 
+
         this.focusScrollViewToToday(scrollViewRef, days_arr, today)
-
-        console.log("mounted")
-
-
     }
 
     focusScrollViewToToday = (scrollViewRef, days_arr, today) => {
         let todayIndex = days_arr.findIndex(obj => obj.dayNumb === today),
-            x_off_set = todayIndex * 60
+            x_off_set
 
-        if(todayIndex >= (days_arr.length -7)){
-            todayIndex = days_arr.length -7
+        if(todayIndex > (days_arr.length - 3)){
+            todayIndex = days_arr.length - 7
             scrollViewRef.scrollTo({
                 y: 0,
-                x: todayIndex * 60
+                x: todayIndex * dayHolderWidth
+            })
+        }
+
+        else if (todayIndex < 3){
+            scrollViewRef.scrollTo({
+                y: 0,
+                x: 0
             })
         }
 
         else{
+            todayIndex -= 3
+            x_off_set = todayIndex * dayHolderWidth
+
             scrollViewRef.scrollTo({
                 y: 0,
                 x: x_off_set
@@ -108,14 +158,16 @@ export default class Daily extends React.Component{
         return new Date(year, month, 0).getDate()
     }
 
-    componentWillUnmount(){
-        console.log("unmount")
-    }
-
     render(){
         return(
             <View style={styles.container}>
-                <ScrollView style={styles.scrollViewContainer} horizontal={true} ref={view => scrollViewRef = view}>
+                <ScrollView style={styles.scrollViewContainer} 
+                    horizontal={true} 
+                    ref={view => scrollViewRef = view}
+                    indicatorStyle='white'
+                    onScroll={this.onScrolling}
+                    scrollEventThrottle={1}
+                >
                     {this.state.dailyTimeView}
                 </ScrollView>
             </View>
@@ -132,9 +184,18 @@ const styles = StyleSheet.create({
     },
 
     dayHolder: {
-        width: 60,
+        width: dayHolderWidth,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+
+    currentDayHolder: {
+        height: 35, //half the size of scrollViewContainer's height
+        width: 35,  //equal to height to form a square
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        borderRadius: 35/2
     }
 })
