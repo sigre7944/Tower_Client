c149fb8b7a272ebc24b592536eab15a8d8cfc306
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-19 16:40:23 +0300

    Update calender month option

--- components/main/bottomTabNavigator/layouts/modal-component/add-task-panel/AddTaskPanel.js
+++ components/main/bottomTabNavigator/layouts/modal-component/add-task-panel/AddTaskPanel.js
@@ -122,7 +122,6 @@ export default class AddTaskPanel extends Component{
 
                     <TouchableHighlight 
                     style={{
-                        // position: 'absolute',
                         width: Dimensions.get('window').width * 1/3,
                         left: Dimensions.get('window').width * 2/3,
                         height: 100,
--- components/main/bottomTabNavigator/layouts/modal-component/calendar/Calendar.js
+++ components/main/bottomTabNavigator/layouts/modal-component/calendar/Calendar.js
@@ -15,6 +15,31 @@ import {
 export default class Calendar extends Component{
 
     state = {
+        currentMonth: 'This month',
+        nextMonthColor: 'white',
+        nextMonthTextColor: 'gray',
+        thisMonthColor: 'black',
+        thisMonthTextColor: 'white'
+    }
+
+    chooseMonthOption = (monthOption) => {
+        if(monthOption === "This month"){
+            this.setState({
+                thisMonthColor: 'black',
+                thisMonthTextColor: 'white',
+                nextMonthColor: 'white',
+                nextMonthTextColor: 'gray'
+            })
+        }
+
+        else if(monthOption === "Next month"){
+            this.setState({
+                thisMonthColor: 'white',
+                thisMonthTextColor: 'gray',
+                nextMonthColor: 'black',
+                nextMonthTextColor: 'white'
+            })
+        }
     }
 
     render(){
@@ -31,7 +56,7 @@ export default class Calendar extends Component{
 
                 }}
             >
-                <View style={{
+                {/* <View style={{
                     height: 80,
                     paddingHorizontal: 30,
                     paddingVertical: 20,
@@ -90,8 +115,62 @@ export default class Calendar extends Component{
                             >Next Monday</Text>
                         </View>
                     </View>
-                </View>
+                </View> */}
                 
+                <View
+                    style={{
+                        height: 80,
+                        paddingHorizontal: 30,
+                        paddingVertical: 20,
+                    }}
+                >
+                    <View 
+                        style={{
+                            height: 35,
+                            borderRadius: 25,
+                            borderWidth: 1,
+                            borderColor: "gainsboro",
+                            flexDirection: "row",
+                        }}
+                    >
+                        <TouchableHighlight 
+                            style={{
+                                backgroundColor: this.state.thisMonthColor,
+                                borderRadius: 25,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                flex: 1,
+                            }}
+                            onPress={this.chooseMonthOption.bind(this, "This month")}
+                            underlayColor="transparent"
+                        >
+                            <Text
+                                style={{
+                                    color: this.state.thisMonthTextColor
+                                }}
+                            >This month</Text>
+                        </TouchableHighlight>
+                        
+                        <TouchableHighlight 
+                            style={{
+                                backgroundColor: this.state.nextMonthColor,
+                                borderRadius: 25,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                flex: 1,
+                            }}
+                            onPress={this.chooseMonthOption.bind(this, "Next month")}    
+                            underlayColor="transparent"
+                        >
+                            <Text
+                                style={{
+                                    color: this.state.nextMonthTextColor
+                                }}
+                            >Next month</Text>
+                        </TouchableHighlight>
+                    </View>
+
+                </View>
             </View>
         )
     }
