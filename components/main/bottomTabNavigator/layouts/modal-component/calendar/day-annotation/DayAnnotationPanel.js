dfd262b7bf17a883c9969a9862d8d3d9f6e6eff6
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-20 13:50:14 +0300

    Update Day Calander

--- components/main/bottomTabNavigator/layouts/modal-component/calendar/day-annotation/DayAnnotationPanel.js
+++ components/main/bottomTabNavigator/layouts/modal-component/calendar/day-annotation/DayAnnotationPanel.js
@@ -47,17 +47,77 @@ export default class DayAnnotationPanel extends Component{
         return new Date(year, month, 0).getDate()
     }
 
+    getDayWordInWeek = (dayInWeek) => {
+        if(dayInWeek === 0){
+            return 'S'
+        }
+
+        else if (dayInWeek === 1){
+            return 'M'
+        }
+
+        else if (dayInWeek === 2){
+            return 'T'
+        }
+
+        else if (dayInWeek === 3){
+            return 'W'
+        }
+
+        else if (dayInWeek === 4){
+            return 'T'
+        }
+
+        else if (dayInWeek === 5){
+            return 'F'
+        }
+
+        else if (dayInWeek === 6){
+            return 'S'
+        }
+    }
+
     componentDidMount(){
         let currentMonth = monthNames[new Date().getMonth()],
             currentYear = new Date().getFullYear()
         
         this.setState({currentMonth, currentYear})
 
-        let daysInMonth = this.getDaysInMonth(new Date().getMonth() + 1, currentYear)
+        let daysInMonth = this.getDaysInMonth(new Date().getMonth() + 1, currentYear),
+            calendar_array = [],
+            display_day_array = [] //length of 7*6 = 42
+
+        let firstDayInWeekOfMonth = new Date(currentYear, currentMonth - 1, 1).getDay(),
+            formerDaysFromLastMonth = firstDayInWeekOfMonth - 1,
+            lastDayInWeekOfMonth = new Date(currentYear, currentMonth - 1, daysInMonth).getDay(),
+            postDaysFromNextMonth = 7 - lastDayInWeekOfMonth
+
+        let daysInLastMonth
+        //To check the current month is January or not
+        if(new Date().getMonth() !== 0)
+            daysInLastMonth = this.getDaysInMonth(new Date().getMonth(), currentYear)
+
+        else
+            daysInLastMonth = this.getDaysInMonth(12, currentYear - 1)
+
+        for(let i = daysInLastMonth - 3; i <= daysInLastMonth; i++){
+            display_day_array.push(i)
+        }
 
         for(let i = 1; i <= daysInMonth; i++){
+            display_day_array.push(i)
             let dayInWeek = new Date(currentYear, currentMonth - 1, i).getDay(),
-                dayText
+                dayWord = this.getDayWordInWeek(dayInWeek)
+
+            
+
+            if(dayInWeek === 1){
+                row_array
+            }
+
+            else{
+
+            }
         }
     }
 
