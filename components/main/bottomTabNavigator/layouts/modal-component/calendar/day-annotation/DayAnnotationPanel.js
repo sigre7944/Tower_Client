be953dfecdd5614caf9f0a2568b0cef60a90d303
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-28 17:09:11 +0300

    Display a year

--- components/main/bottomTabNavigator/layouts/modal-component/calendar/day-annotation/DayAnnotationPanel.js
+++ components/main/bottomTabNavigator/layouts/modal-component/calendar/day-annotation/DayAnnotationPanel.js
@@ -18,7 +18,8 @@ export default class DayAnnotationPanel extends Component{
 
     state = {
         renderDaysInMonth: null,
-        currentIndexOfTotalCalendarMonth: 0
+        currentIndexOfTotalCalendarMonth: 0,
+        monthComponent_arr: null
     }
 
     chooseDiffCalendarMonth = (index) => {
@@ -27,14 +28,105 @@ export default class DayAnnotationPanel extends Component{
         })
     }
 
-    componentDidMount(){
-        
+    centerlizeCurrentMonth = (currentMonth, currentYear) => {
+        let monthsToDisplay_arr = []
+
+        //get the past 5 months and current month (6 in total)
+        //if current month is a month in between January - April
+        if(currentMonth - 5 < 0){
+            let monthsFromLastYear = 5 - currentMonth
+
+            //Get the data of months from the last year
+            for(let i = 11 - (monthsFromLastYear) + 1; i <= 11; i++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear - 1
+                })
+            }
+
+            //Get the data of past months in the current year
+            for(let i = 0; i <= currentMonth; i ++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear
+                })
+            }
+        }
+
+        else {
+            //Get the data of past months in the current year
+            for(let i = 0; i <= currentMonth; i ++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear
+                })
+            }
+        }
+
+        //Get the next 6 months
+        //if current month is a month in between July - December
+        if(11 - currentMonth < 6){
+            let monthsInNextYear = 6 - (11 - currentMonth)
+
+            //Get the data of next months in the current year
+            for(let i = currentMonth + 1; i <= 11; i++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear,
+                })
+            }
+
+            //Get the data of months in the next year
+            for(let i = 0; i < (monthsInNextYear - 1); i++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear + 1,
+                })
+            }
+        }
+
+        else {
+            //Get the data of next months in the current year
+            for(let i = currentMonth + 1; i <= 11; i++){
+                monthsToDisplay_arr.push({
+                    month: i,
+                    year: currentYear,
+                })
+            }
+        }
+
+        this.setState({
+            monthComponent_arr: monthsToDisplay_arr.map((data, index) => (
+                <CalendarDisplayHolder 
+                key={'month-render-calendar' + index}
+                style={{
+                    flex: 1,
+                    width: Dimensions.get('window').width - 50,
+                    marginRight: (Dimensions.get('window').width - 50)
+                }} 
+
+                month={data.month} 
+                year={data.year}
+
+                chooseDiffCalendarMonth = {this.chooseDiffCalendarMonth}
+                currentIndexOfTotalCalendarMonth = {this.state.currentIndexOfTotalCalendarMonth}
+                calendarIndex = {index}
+                />
+            ))
+        })
     }
 
+    componentDidMount(){
+        let currentMonth = new Date().getMonth(),
+            currentYear = new Date().getFullYear()
+
+        this.centerlizeCurrentMonth(currentMonth, currentYear)
+    }
 
     render(){
         return(
             <>
+            {/* Today Tommorow Next Monday */}
             <View style={{
                 height: 80,
                 paddingHorizontal: 30,
@@ -97,7 +189,7 @@ export default class DayAnnotationPanel extends Component{
                 </View>
             </View> 
 
-
+            {/* Main content of calendar */}
             <View 
                 style = {{
                     flex: 1,
@@ -113,51 +205,44 @@ export default class DayAnnotationPanel extends Component{
 
                     }}
                 >
-                    <CalendarDisplayHolder 
-                    style={{
-                        flex: 1,
-                        width: Dimensions.get('window').width - 50,
-                        marginRight: (Dimensions.get('window').width - 50)
-                    }} 
 
-                    month={new Date().getMonth()} 
-                    year={new Date().getFullYear()}
-
-                    chooseDiffCalendarMonth = {this.chooseDiffCalendarMonth}
-                    currentIndexOfTotalCalendarMonth = {this.state.currentIndexOfTotalCalendarMonth}
-                    calendarIndex = {0}
-                    />
-
-                    <CalendarDisplayHolder 
-                    style={{
-                        flex: 1,
-                        width: Dimensions.get('window').width - 50,
-                        marginRight: (Dimensions.get('window').width - 50)
-                    }} 
-                    month={(new Date().getMonth() + 1) > 11 ? 1 : new Date().getMonth() + 1 } 
-                    year={(new Date().getMonth() + 1) > 11 ? new Date().getFullYear() + 1 : new Date().getFullYear()}
-
-                    chooseDiffCalendarMonth = {this.chooseDiffCalendarMonth}
-                    currentIndexOfTotalCalendarMonth = {this.state.currentIndexOfTotalCalendarMonth}
-                    calendarIndex = {1}
-                    />
-                    
-                    <CalendarDisplayHolder 
-                    style={{
-                        flex: 1,
-                        width: Dimensions.get('window').width - 50,
-                    }} 
-                    month={(new Date().getMonth() + 2) > 11 ? 1 : new Date().getMonth() + 2 } 
-                    year={(new Date().getMonth() + 2) > 11 ? new Date().getFullYear() + 1 : new Date().getFullYear()}
-
-                    chooseDiffCalendarMonth = {this.chooseDiffCalendarMonth}
-                    currentIndexOfTotalCalendarMonth = {this.state.currentIndexOfTotalCalendarMonth}
-                    calendarIndex = {2}
-                    />
+                    {this.state.monthComponent_arr}
 
                 </ScrollView>
             </View>
+            
 
+            <View
+                style={{
+                    height: 50,
+                    backgroundColor: 'white',
+                    borderTopWidth: 1,
+                    borderBottomWidth: 1,
+                    borderBottomColor: 'gainsboro',
+                    borderTopColor: 'gainsboro',
+                }}
+            >
+                <Text>
+                    Add time
+                </Text>
+            </View>
+            <View
+                style={{
+                    height: 50,
+                    backgroundColor: 'yellow'
+                }}
+            >
+
+            </View>
+            <View
+                style={{
+                    height: 60,
+                    backgroundColor: 'black',
+                    marginBottom: 20,
+                }}
+            >
+
+            </View>
             </>
         )
     }
