92e4dc22fd9ae3e3cd7787ecb116afb867d5e129
minhchinh97h <minhchinhduong97@gmail.com>
2019-07-30 01:26:52 +0300

    load asynchronously categories from file

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
 import React from 'react';
 import MainNavigator from './components/main/Main' //Main screen
-import {createStackNavigator, createAppContainer, createDrawerNavigator} from 'react-navigation'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
+import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import Drawer from './components/drawer/Drawer'
 import Header from './components/main/header/Header'
 
-const store = createStore(rootReducer)
+import * as FileSystem from 'expo-file-system';
+
+let categories = {},
+  filePath = FileSystem.documentDirectory + "categories.json",
+  store
+
+
+
+
+// const store = createStore(rootReducer)
 
 export default class App extends React.Component {
+
+  state = {
+    store: undefined,
+  }
+
+  LoadCategoriesFromFile = (filePath) => {
+    FileSystem.getInfoAsync(filePath)
+    .then(data => {
+      if (data.exists) {
+        FileSystem.readAsStringAsync(filePath)
+          .then(data => {
+            categories = JSON.parse(data)
+            
+            this.setState({
+              store: createStore(rootReducer, { categories })
+            })
+          })
+      }
+  
+      else {
+  
+        FileSystem.writeAsStringAsync(
+          filePath,
+          JSON.stringify({
+            "cate_0": {
+              "name": "Inbox",
+              "color": "red"
+            }
+          })
+        )
+          .then(() => {
+            return FileSystem.readAsStringAsync(filePath)
+  
+  
+          })
+          .then(data => {
+            initialState = JSON.parse(data)
+  
+            this.setState({
+              store: createStore(rootReducer, { categories })
+            })
+          })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  componentDidMount(){
+    this.LoadCategoriesFromFile(filePath)
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <AppContainer />
-      </Provider>
+      <>
+        {
+          this.state.store ?
+            <Provider store={this.state.store}>
+              <AppContainer />
+            </Provider>
+
+          :
+
+          null
+        }
+      </>
+
     )
   }
 }
@@ -22,11 +94,11 @@ export default class App extends React.Component {
 const ContentNavigator = createStackNavigator(
   { //Stack navigator works as a history object in a web browser, which helps popping out in pushing in screen to proceed navigations
     Main: MainNavigator
-  }, 
+  },
   {
     initialRouteName: "Main",
-    defaultNavigationOptions: ({navigation}) => ({
-      header: <Header navigation = {navigation} />
+    defaultNavigationOptions: ({ navigation }) => ({
+      header: <Header navigation={navigation} />
     })
   }
 )
@@ -34,8 +106,13 @@ const ContentNavigator = createStackNavigator(
 const drawerNavigator = createDrawerNavigator({
   ContentNavigator: ContentNavigator
 }, {
-  drawerLockMode: 'locked-closed',
-  contentComponent: Drawer
-})
+    drawerLockMode: 'locked-closed',
+    contentComponent: Drawer
+  })
 
-const AppContainer =  createAppContainer(drawerNavigator) //return a React component, which is to wrap the stack navigator 
+const AppContainer = createAppContainer(drawerNavigator) //return a React component, which is to wrap the stack navigator 
+
+
+
+
+
--- reducers/categories.js
+++ reducers/categories.js
@@ -1,48 +1,72 @@
 
 import * as FileSystem from 'expo-file-system';
 
-let initialState = {},
-    filePath = FileSystem.documentDirectory + "categories.json"
+let initialState = {}
+//     filePath = FileSystem.documentDirectory + "categories.json"
+
+// export let categories
 
 
-FileSystem.getInfoAsync(filePath)
-    .then(data => {
-        if (data.exists) {
-            FileSystem.readAsStringAsync(filePath)
-                .then(data => {
-                    initialState = JSON.parse(data)
-                })
-        }
+// FileSystem.getInfoAsync(filePath)
+//     .then(data => {
+//         if (data.exists) {
+//             FileSystem.readAsStringAsync(filePath)
+//                 .then(data => {
+//                     initialState = JSON.parse(data)
 
-        else {
+//                     console.log(initialState)
+//                 })
+//         }
 
-            FileSystem.writeAsStringAsync(
-                filePath,
-                JSON.stringify({
-                    "cate_0": {
-                        "name": "Inbox",
-                        "color": "red"
-                    }
-                })
-            )
-                .then(() => {
-                    return FileSystem.readAsStringAsync(filePath)
-                })
-                .then(data => {
-                    initialState = JSON.parse(data)
-                })
-        }
-    })
-    .catch(err => {
-        console.log(err)
-    })
+//         else {
+
+//             FileSystem.writeAsStringAsync(
+//                 filePath,
+//                 JSON.stringify({
+//                     "cate_0": {
+//                         "name": "Inbox",
+//                         "color": "red"
+//                     }
+//                 })
+//             )
+//                 .then(() => {
+//                     return FileSystem.readAsStringAsync(filePath)
+
+
+//                 })
+//                 .then(data => {
+//                     initialState = JSON.parse(data)
+
+//                     categories = (state = initialState, action) => {
+//                         switch (action.type) {
+//                             case 'CREATE_CATEGORY':
+//                                 return { ...state, ...action.data }
+
+//                             default:
+//                                 if (state === undefined) {
+//                                     return initialState
+//                                 }
+
+//                                 return state
+//                         }
+//                     }
+
+//                 })
+//         }
+//     })
+//     .catch (err => {
+//     console.log(err)
+// })
 
 export const categories = (state = initialState, action) => {
+
     switch (action.type) {
         case 'CREATE_CATEGORY':
             return { ...state, ...action.data }
 
         default:
+
             return state
     }
 }
+
