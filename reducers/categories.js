dc02d2fd633d985a36511cc38a41803ae133b795
minhchinh97h <minhchinhduong97@gmail.com>
2019-07-30 01:02:51 +0300

    Organize files and apply expo-file-system

--- reducers/categories.js
+++ reducers/categories.js
@@ -1,23 +1,41 @@
 
+import * as FileSystem from 'expo-file-system';
+
+let initialState = {},
+    filePath = FileSystem.documentDirectory + "categories.json"
 
 
-let initialState= {}
+FileSystem.getInfoAsync(filePath)
+    .then(data => {
+        if (data.exists) {
+            FileSystem.readAsStringAsync(filePath)
+                .then(data => {
+                    initialState = JSON.parse(data)
+                })
+        }
 
-// fs.('../data')
-//     .then((data) => {
-//         try {
-//             // initialState = JSON.parse(data)
+        else {
 
-//             console.log(data)
-//         }
-//         catch (err) {
-//             console.log(err)
-//         }
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-
+            FileSystem.writeAsStringAsync(
+                filePath,
+                JSON.stringify({
+                    "cate_0": {
+                        "name": "Inbox",
+                        "color": "red"
+                    }
+                })
+            )
+                .then(() => {
+                    return FileSystem.readAsStringAsync(filePath)
+                })
+                .then(data => {
+                    initialState = JSON.parse(data)
+                })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 export const categories = (state = initialState, action) => {
     switch (action.type) {
