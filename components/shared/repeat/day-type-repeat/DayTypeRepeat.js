4e4fa8c65ff8931b5ee3257e601fa7b206e041ce
minhchinh97h <minhchinhduong97@gmail.com>
2019-10-19 12:58:09 +0300

    Working on date picker for repeat

--- components/shared/repeat/day-type-repeat/DayTypeRepeat.js
+++ components/shared/repeat/day-type-repeat/DayTypeRepeat.js
@@ -20,7 +20,10 @@ import {
 import { styles } from './styles/styles'
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faRedoAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+    faRedoAlt,
+    faHourglassEnd
+} from '@fortawesome/free-solid-svg-icons'
 
 const shortMonthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
 const animation_duration = 250
@@ -228,15 +231,22 @@ export default class DayTypeRepeat extends React.PureComponent {
 
                 <View
                     style={{
-                        marginTop: 26,
+                        marginTop: 25,
                         flexDirection: "row",
                         alignItems: "center",
-                        marginLeft: 30,
-                        marginRight: 30,
+                        marginHorizontal: 30,
                     }}
                 >
                     <ChooseDayInWeekOption />
                 </View>
+
+                {/* Separating line */}
+                <View
+                    style={styles.separating_line}
+                >
+                </View>
+
+                <RepeatEndOptionsHolder />
             </Animated.View>
         )
     }
@@ -285,22 +295,13 @@ class DayInWeekOptionHolder extends React.PureComponent {
 
     handleIndex = (index) => {
         if (index === 0) {
-            // this.setState({
-            //     container_style: styles.unchosen_left_end_day_in_week_container,
-            // })
             return styles.unchosen_left_end_day_in_week_container
         }
 
         else if (index === 6) {
-            // this.setState({
-            //     container_style: styles.unchosen_right_end_day_in_week_container,
-            // })
             return styles.unchosen_right_end_day_in_week_container
         }
         else {
-            // this.setState({
-            //     container_style: styles.unchosen_normal_day_in_week_container,
-            // })
             return styles.unchosen_normal_day_in_week_container
         }
     }
@@ -412,6 +413,451 @@ class DayInWeekOptionHolder extends React.PureComponent {
                     </>
                 }
             </>
+        )
+    }
+}
+
+class RepeatEndOptionsHolder extends React.PureComponent {
+
+    state = {
+        current_index: 0,
+        last_index: -1
+    }
+
+    chooseEndOption = (index) => {
+        if (this.state.current_index !== index) {
+            this.setState(prevState => ({
+                current_index: index,
+                last_index: prevState.current_index
+            }))
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        marginLeft: 30,
+                        marginTop: 25,
+                        alignItems: "center"
+                    }}
+                >
+                    <FontAwesomeIcon
+                        icon={faHourglassEnd}
+                        color="#2C2C2C"
+                        size={14}
+                    />
+
+                    <Text
+                        style={styles.title_text}
+                    >
+                        Repeat end
+                    </Text>
+                </View>
+
+                <RepeatEndNeverOptionRow
+                    index={0}
+                    chooseEndOption={this.chooseEndOption}
+                    current_index={this.state.current_index}
+                    last_index={this.state.last_index}
+                />
+
+                <RepeatEndOnOptionRow
+                    index={1}
+                    chooseEndOption={this.chooseEndOption}
+                    current_index={this.state.current_index}
+                    last_index={this.state.last_index}
+                />
+            </View>
+        )
+    }
+}
+
+class RepeatEndNeverOptionRow extends React.Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.props.index === nextProps.current_index && this.props.current_index !== nextProps.current_index)
+            || (this.props.index === nextProps.last_index && this.props.last_index !== nextProps.last_index)
+    }
+
+    _chooseEndOption = () => {
+        this.props.chooseEndOption(this.props.index)
+    }
+
+    render() {
+        let text_style = styles.unchosen_every_option_text,
+            button_container = styles.repeat_end_chosen_button_container_deactivated,
+            activated = this.props.index === this.props.current_index
+
+        if (this.props.index === this.props.current_index) {
+            text_style = styles.every_option_text
+            button_container = styles.repeat_end_chosen_button_container
+        }
+
+        else if (this.props.index === this.props.last_index) {
+            text_style = styles.unchosen_every_option_text
+            button_container = styles.repeat_end_chosen_button_container_deactivated
+        }
+
+        return (
+            <TouchableOpacity
+                style={{
+                    marginTop: 25,
+                    marginLeft: 59,
+                    marginRight: 30,
+                }}
+
+                onPress={this._chooseEndOption}
+            >
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                    }}
+                >
+                    <Text
+                        style={text_style}
+                    >
+                        Never
+                        </Text>
+
+                    <View
+                        style={button_container}
+                    >
+                        {activated ?
+                            <View
+                                style={styles.repeat_end_chosen_button_activated}
+                            >
+
+                            </View>
+                            :
+                            null
+                        }
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+class RepeatEndOnOptionRow extends React.Component {
+
+    date = new Date()
+
+    state = {
+        is_date_picker_chosen: false,
+        day: this.date.getDate().toString(),
+        month: this.date.getMonth().toString(),
+        year: this.date.getFullYear().toString()
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (this.props.index === nextProps.current_index && this.props.current_index !== nextProps.current_index)
+            || (this.props.index === nextProps.last_index && this.props.last_index !== nextProps.last_index)
+            || (this.state !== nextState)
+    }
+
+    _chooseEndOption = () => {
+        this.props.chooseEndOption(this.props.index)
+        this.setState({
+            is_date_picker_chosen: true
+        })
+    }
+
+    closeDatePicker = () => {
+        this.setState({
+            is_date_picker_chosen: false
+        })
+    }
+
+    _changeDayPickerValue = (itemValue, itemIndex) => {
+        let year = parseInt(this.state.year),
+            month = parseInt(this.state.month),
+            day = parseInt(itemValue)
+
+        let date = new Date(year, month, day)
+
+        if (date.getMonth() !== month) {
+            date = new Date(year, month + 1, 0)
+        }
+
+        this.setState({
+            day: date.getDate().toString()
+        })
+    }
+
+    _changeMonthPickerValue = (itemValue, itemIndex) => {
+        this.setState({
+            month: itemValue
+        })
+    }
+
+    _changeYearPickerValue = (itemValue, itemIndex) => {
+        this.setState({
+            year: itemValue
+        })
+    }
+
+
+    render() {
+        let text_style = styles.unchosen_every_option_text,
+            button_container = styles.repeat_end_chosen_button_container_deactivated,
+            activated = this.props.index === this.props.current_index
+
+        if (this.props.index === this.props.current_index) {
+            text_style = styles.every_option_text
+            button_container = styles.repeat_end_chosen_button_container
+        }
+
+        else if (this.props.index === this.props.last_index) {
+            text_style = styles.unchosen_every_option_text
+            button_container = styles.repeat_end_chosen_button_container_deactivated
+        }
+
+        return (
+            <>
+                <TouchableOpacity
+                    style={{
+                        marginTop: 25,
+                        marginLeft: 59,
+                        marginRight: 30,
+                    }}
+
+                    onPress={this._chooseEndOption}
+                >
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                        <View
+                            style={{
+                                flexDirection: "row",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Text
+                                style={text_style}
+                            >
+                                On
+                        </Text>
+
+                            <View
+                                style={{
+                                    marginLeft: 20,
+                                }}
+                            >
+                                <Text
+                                    style={text_style}
+                                >
+
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View
+                            style={button_container}
+                        >
+                            {activated ?
+                                <View
+                                    style={styles.repeat_end_chosen_button_activated}
+                                >
+
+                                </View>
+                                :
+                                null
+                            }
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <Modal
+                    transparent={true}
+                    visible={this.state.is_date_picker_chosen}
+                >
+                    <View
+                        style={{
+                            flex: 1,
+                            position: "relative"
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                flex: 1,
+                                width: window_width,
+                                backgroundColor: "black",
+                                opacity: 0.2
+                            }}
+
+                            onPress={this.closeDatePicker}
+                        >
+
+                        </TouchableOpacity>
+
+                        <DatePickerWheel
+                            day={this.state.day}
+                            month={this.state.month}
+                            year={this.state.year}
+                            _changeDayPickerValue={this._changeDayPickerValue}
+                            _changeMonthPickerValue={this._changeMonthPickerValue}
+                            _changeYearPickerValue={this._changeYearPickerValue}
+                        />
+                    </View>
+                </Modal>
+            </>
+        )
+    }
+}
+
+class DatePickerWheel extends React.PureComponent {
+
+    render() {
+        return (
+            <View
+                style={{
+                    position: "absolute",
+                    height: 200,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    backgroundColor: "white",
+                    justifyContent: "center",
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                }}
+            >
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                >
+                    <DayPickerValueHolder
+                        day={this.props.day}
+                        _changeDayPickerValue={this.props._changeDayPickerValue}
+                    />
+                    <MonthPickerValueHolder
+                        month={this.props.month}
+                        _changeMonthPickerValue={this.props._changeMonthPickerValue}
+                    />
+                    <YearPickerValueHolder
+                        year={this.props.year}
+                        _changeYearPickerValue={this.props._changeYearPickerValue}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+class DayPickerValueHolder extends React.PureComponent {
+
+    render() {
+        return (
+            <Picker
+                selectedValue={this.props.day}
+                onValueChange={this.props._changeDayPickerValue}
+                itemStyle={styles.picker_value_text}
+                style={{
+                    height: 50,
+                    flex: 1,
+                    justifyContent: "center"
+                }}
+            >
+                <Picker.Item label="1" value="1" />
+                <Picker.Item label="2" value="2" />
+                <Picker.Item label="3" value="3" />
+                <Picker.Item label="4" value="4" />
+                <Picker.Item label="5" value="5" />
+                <Picker.Item label="6" value="6" />
+                <Picker.Item label="7" value="7" />
+                <Picker.Item label="8" value="8" />
+                <Picker.Item label="9" value="9" />
+                <Picker.Item label="10" value="10" />
+                <Picker.Item label="11" value="11" />
+                <Picker.Item label="12" value="12" />
+                <Picker.Item label="13" value="13" />
+                <Picker.Item label="14" value="14" />
+                <Picker.Item label="15" value="15" />
+                <Picker.Item label="16" value="16" />
+                <Picker.Item label="17" value="17" />
+                <Picker.Item label="18" value="18" />
+                <Picker.Item label="19" value="19" />
+                <Picker.Item label="20" value="20" />
+                <Picker.Item label="21" value="21" />
+                <Picker.Item label="22" value="22" />
+                <Picker.Item label="23" value="23" />
+                <Picker.Item label="24" value="24" />
+                <Picker.Item label="25" value="25" />
+                <Picker.Item label="26" value="26" />
+                <Picker.Item label="27" value="27" />
+                <Picker.Item label="28" value="28" />
+                <Picker.Item label="29" value="29" />
+                <Picker.Item label="30" value="30" />
+                <Picker.Item label="31" value="31" />
+            </Picker>
+        )
+    }
+}
+
+class MonthPickerValueHolder extends React.PureComponent {
+
+    render() {
+        return (
+            <Picker
+                selectedValue={this.props.month}
+                onValueChange={this.props._changeMonthPickerValue}
+                itemStyle={styles.picker_value_text}
+                style={{
+                    height: 50,
+                    flex: 1,
+                    justifyContent: "center"
+                }}
+            >
+                <Picker.Item label="January" value="0" />
+                <Picker.Item label="Febuary" value="1" />
+                <Picker.Item label="March" value="2" />
+                <Picker.Item label="April" value="3" />
+                <Picker.Item label="May" value="4" />
+                <Picker.Item label="June" value="5" />
+                <Picker.Item label="July" value="6" />
+                <Picker.Item label="August" value="7" />
+                <Picker.Item label="September" value="8" />
+                <Picker.Item label="October" value="9" />
+                <Picker.Item label="November" value="10" />
+                <Picker.Item label="December" value="11" />
+            </Picker>
+        )
+    }
+}
+
+class YearPickerValueHolder extends React.PureComponent {
+    year = new Date().getFullYear()
+    render() {
+        return (
+            <Picker
+                selectedValue={this.props.year}
+                onValueChange={this.props._changeYearPickerValue}
+                itemStyle={styles.picker_value_text}
+                style={{
+                    height: 50,
+                    flex: 1,
+                    justifyContent: "center"
+                }}
+            >
+                <Picker.Item label={`${this.year}`} value={`${this.year}`} />
+                <Picker.Item label={`${this.year + 1}`} value={`${this.year + 1}`} />
+                <Picker.Item label={`${this.year + 2}`} value={`${this.year + 2}`} />
+                <Picker.Item label={`${this.year + 3}`} value={`${this.year + 3}`} />
+                <Picker.Item label={`${this.year + 4}`} value={`${this.year + 4}`} />
+            </Picker>
         )
     }
 }
--- components/shared/repeat/day-type-repeat/styles/styles.js
+++ components/shared/repeat/day-type-repeat/styles/styles.js
@@ -23,6 +23,14 @@ export const styles = StyleSheet.create({
         color: CommonStyle.primary_colors.prim_1
     },
 
+    unchosen_every_option_text: {
+        fontFamily: CommonStyle.sf_ui_display_light_font,
+        fontSize: 15,
+        lineHeight: 18,
+        letterSpacing: -0.02,
+        color: CommonStyle.text_icon_colors.ti_3
+    },
+
     every_option_input: {
         fontFamily: CommonStyle.sf_ui_display_light_font,
         fontSize: 15,
@@ -145,6 +153,41 @@ export const styles = StyleSheet.create({
         lineHeight: 14,
         letterSpacing: -0.02,
         color: "white"
-    }
+    },
 
+    separating_line: {
+        marginTop: 25,
+        height: 1,
+        backgroundColor: CommonStyle.text_icon_colors.ti_4,
+        marginHorizontal: 30,
+    },
+
+    repeat_end_chosen_button_container: {
+        width: 16,
+        height: 16,
+        borderRadius: 16,
+        borderWidth: 1.5,
+        borderColor: CommonStyle.primary_colors.prim_1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white"
+    },
+
+    repeat_end_chosen_button_activated: {
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+        backgroundColor: CommonStyle.primary_colors.prim_1
+    },
+
+    repeat_end_chosen_button_container_deactivated: {
+        width: 16,
+        height: 16,
+        borderRadius: 16,
+        borderWidth: 1.5,
+        borderColor: CommonStyle.text_icon_colors.ti_3,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white"
+    }
 })
