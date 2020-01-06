d07f413d63526fab4e6158d4303cfe3054683cf8
hanabusa <sigre7944@gmail.com>
2020-01-06 18:08:40 +0200

    Commit changes to continue later

---
+++ components/push-notification/PushNotification.Container.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import PushNotification from "./PushNotification";
+
+const mapStateToProps = state => {
+  return({
+      
+  })
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PushNotification);
